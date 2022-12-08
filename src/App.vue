cf457d46eef0e4df5c311028cfae098f2b2d7051
Sophia Pache <sophpache@gmail.com>
2022-12-08 17:29:35 +1100

    updated app layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
     <div class="ui inverted segment navbar">
-      <div class="ui center aligned container">
-        <div class="ui large secondary inverted pointing menu compact">
-          <router-link to="/" exact class="item">
-            <i class="home icon" /> Home
-          </router-link>
-          <router-link to="/signup" class="item">
-            <i class="newspaper outline icon" /> Newsletter
-          </router-link>
-        </div>
+      <div class="ui large secondary inverted pointing menu compact">
+        <router-link to="/" exact class="item">
+          <i class="home icon" /> Home
+        </router-link>
+        <router-link to="/signup" class="item">
+          <i class="newspaper outline icon" /> Newsletter
+        </router-link>
       </div>
     </div>
     <div id="image"></div>
@@ -30,23 +28,45 @@
   margin: 0;
   padding: 0;
   font-family: "Iceberg", cursive;
+  background-color: #1e1e1e;
+  color: white;
+}
+
+body {
+  background-color: #1e1e1e;
 }
 #image {
   width: 100vw;
-  height: 48vh;
-  padding: 0 0;
-  background: url(https://i.ibb.co/V3ymWym/hero-banner-1.png);
-  background-size: contain;
+  height: 23vw;
+  background: url(https://i.imgur.com/Vao4LFW.jpeg);
+  background-size: cover;
   background-repeat: no-repeat;
+  margin-top: -17px;
+  border-bottom: 0.5em solid white;
 }
 
 div.ui.inverted.segment {
   background: black;
   padding: 0 auto;
+  height: 8vh;
 }
 
 div.ui.secondary.pointing.menu.item a {
   text-align: center;
+}
+
+.border {
+  position: absolute;
+  width: 100vw;
+  height: 9px;
+  left: 0px;
+  top: 45%;
+  background: #f7f7f7;
+}
+
+div.ui.text.container {
+  height: 100vh;
+  margin-top: 20px;
 }
 </style>
 
