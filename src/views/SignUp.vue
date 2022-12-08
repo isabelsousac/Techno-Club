236e3dd472e6cf8e1818a3df38b6e382c5736086
isabelsousac <isabel.sousa15246@gmail.com>
2022-12-08 20:11:19 +1100

    Makes form for newsletter's sign up

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,35 +1,155 @@
 <template>
-  <div class="about">
-    <h1>This is a sign up page</h1>
+  <div class="ui raised very padded text container ">
+    <h1>Newsletter</h1>
+    <p>Be one of the first club rats to have access to the hardest music events and hardest drugs in the scene.</p>
+
+    <h3>PERSONAL DETAILS</h3>
 
     <form>
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
+        <label for="exampleInputPassword1">FIRST NAME</label>
+        <input
+          type="text"
+          class="form-control"
+          id="first-name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">LAST NAME</label>
+        <input
+          type="text"
+          class="form-control"
+          id="last-name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">EMAIL ADRESS</label>
         <input
           type="email"
           class="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
-          placeholder="Enter email"
         />
-        <small id="emailHelp" class="form-text text-muted"
-          >We'll never share your email with anyone else.</small
-        >
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">Name</label>
+        <label for="exampleInputPassword1">PHONE NUMBER</label>
         <input
           type="text"
           class="form-control"
           id="exampleInputPassword1"
-          placeholder="Name"
         />
       </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+      <h3>KEEP INFORMED</h3>
+
+      <div class="ui segment">
+        <div class="field">
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="events" tabindex="0" class="hidden">
+            <label>EVENTS</label>
+          </div>
+        </div>
       </div>
-      <button type="submit" class="btn btn-dark">Submit</button>
+
+      <div class="ui segment">
+        <div class="field">
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="news" tabindex="0" class="hidden">
+            <label>NEWS</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="ui segment">
+        <div class="field">
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="discounts" tabindex="0" class="hidden">
+            <label>DISCOUNTS</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="ui segment">
+        <div class="field">
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="covid" tabindex="0" class="hidden">
+            <label>COVID-19</label>
+          </div>
+        </div>
+      </div>
+
+      <h3>HOW WE'LL REACH YOU</h3>
+
+      <div class="ui segment">
+        <div class="field">
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="sms" tabindex="0" class="hidden">
+            <label>VIA SMS</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="ui segment">
+        <div class="field">
+          <div class="ui toggle checkbox">
+            <input type="checkbox" name="email" tabindex="0" class="hidden">
+            <label>VIA EMAIL</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="subscribe">
+        <button type="submit" class="btn btn-primary">SUBSCRIBE</button>
+        <p>Our privacy policy</p>
+      </div>
     </form>
   </div>
 </template>
+
+<script>
+  // import jQuery from 'jQuery'
+
+  // const $ = jQuery;
+
+  // $('.ui.checkbox').checkbox();
+</script>
+
+<style scoped>
+  h1, h3 {
+    font-family: "Iceberg", cursive;
+  }
+
+  .form-control {
+    background: rgba(247, 247, 247, 0.3);
+    height: 50px;
+  }
+
+  .ui.segment {
+    background: #1e1e1e;
+  }
+
+  .ui.toggle.checkbox label {
+    color: white;
+  }
+
+  p{
+    font-size: 13px;
+  }
+
+  p[data-v-6f83b81c] {
+    font-size: 16px;
+}
+
+  .subscribe {
+    text-align: center;
+    padding-bottom: 15px;
+  }
+
+  .subscribe button {
+    width: 208px;
+    height: 52px;
+    background: #F6F6F6;
+    color: black;
+  }
+
+</style>
