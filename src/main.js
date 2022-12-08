65e175fbf57db4fec951e768147c663859d8e3a9
isabelsousac <isabel.sousa15246@gmail.com>
2022-12-08 15:02:39 +1100

    Container css

--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ import router from "./router";
 import "semantic-ui-css/semantic.css";
 Vue.config.productionTip = false;
 
+<<<<<<< Updated upstream
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+=======
+Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'semantic-ui-css/semantic.css'; // WILL THIS WORK?
+
+>>>>>>> Stashed changes
 
 new Vue({
   router,
