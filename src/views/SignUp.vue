f708fce88c60d69b1e5d28411f13dbae70615872
Sophia Pache <sophpache@gmail.com>
2022-12-09 12:39:33 +1100

    added form banner

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,125 +1,133 @@
 <template>
-  <div class="ui raised very padded text container">
-    <h1>Newsletter</h1>
-    <p>
-      Be one of the first club rats to have access to the hardest music events
-      and hardest drugs in the scene.
-    </p>
+  <div>
+    <img src="https://i.imgur.com/EQW8FRE.png" class="newsBanner" />
+    <div class="ui raised very padded text container">
+      <h1>Newsletter</h1>
+      <p>
+        Be one of the first club rats to have access to the hardest music events
+        and hardest drugs in the scene.
+      </p>
 
-    <h3>PERSONAL DETAILS</h3>
+      <h3>PERSONAL DETAILS</h3>
 
-    <form @submit.prevent="submit">
-      <p v-if="errorsPresent" class="error">Please fill out both fields</p>
-      <div class="form-group">
-        <label for="exampleInputPassword1">FIRST NAME</label>
-        <input
-          type="text"
-          class="form-control"
-          id="first-name"
-          v-model="firstName"
-        />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">LAST NAME</label>
-        <input
-          type="text"
-          class="form-control"
-          id="last-name"
-          v-model="lastName"
-        />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputEmail1">EMAIL ADDRESS</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          v-model="email"
-          :rules="emailRules"
-        />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">PHONE NUMBER</label>
-        <input type="text" class="form-control" id="exampleInputPassword1" />
-      </div>
+      <form @submit.prevent="submit">
+        <p v-if="errorsPresent" class="error">Please fill out both fields</p>
+        <div class="form-group">
+          <label for="exampleInputPassword1">FIRST NAME</label>
+          <input
+            type="text"
+            class="form-control"
+            id="first-name"
+            v-model="firstName"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">LAST NAME</label>
+          <input
+            type="text"
+            class="form-control"
+            id="last-name"
+            v-model="lastName"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">EMAIL ADDRESS</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            v-model="email"
+            :rules="emailRules"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">PHONE NUMBER</label>
+          <input type="text" class="form-control" id="exampleInputPassword1" />
+        </div>
 
-      <h3>KEEP INFORMED</h3>
+        <h3>KEEP INFORMED</h3>
 
-      <div class="ui segment">
-        <div class="field">
-          <div class="ui toggle checkbox">
-            <input type="checkbox" name="events" tabindex="0" class="hidden" />
-            <label>EVENTS</label>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui toggle checkbox">
+              <input
+                type="checkbox"
+                name="events"
+                tabindex="0"
+                class="hidden"
+              />
+              <label>EVENTS</label>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="ui segment">
-        <div class="field">
-          <div class="ui toggle checkbox">
-            <input type="checkbox" name="news" tabindex="0" class="hidden" />
-            <label>NEWS</label>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui toggle checkbox">
+              <input type="checkbox" name="news" tabindex="0" class="hidden" />
+              <label>NEWS</label>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="ui segment">
-        <div class="field">
-          <div class="ui toggle checkbox">
-            <input
-              type="checkbox"
-              name="discounts"
-              tabindex="0"
-              class="hidden"
-            />
-            <label>DISCOUNTS</label>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui toggle checkbox">
+              <input
+                type="checkbox"
+                name="discounts"
+                tabindex="0"
+                class="hidden"
+              />
+              <label>DISCOUNTS</label>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="ui segment">
-        <div class="field">
-          <div class="ui toggle checkbox">
-            <input type="checkbox" name="covid" tabindex="0" class="hidden" />
-            <label>COVID-19</label>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui toggle checkbox">
+              <input type="checkbox" name="covid" tabindex="0" class="hidden" />
+              <label>COVID-19</label>
+            </div>
           </div>
         </div>
-      </div>
 
-      <h3>HOW WE'LL REACH YOU</h3>
+        <h3>HOW WE'LL REACH YOU</h3>
 
-      <div class="ui segment">
-        <div class="field">
-          <div class="ui toggle checkbox">
-            <input
-              type="checkbox"
-              name="sms"
-              tabindex="0"
-              class="hidden"
-              v-model="sms"
-            />
-            <label>VIA SMS</label>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui toggle checkbox">
+              <input
+                type="checkbox"
+                name="sms"
+                tabindex="0"
+                class="hidden"
+                v-model="sms"
+              />
+              <label>VIA SMS</label>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="ui segment">
-        <div class="field">
-          <div class="ui toggle checkbox">
-            <input type="checkbox" name="email" tabindex="0" class="hidden" />
-            <label>VIA EMAIL</label>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui toggle checkbox">
+              <input type="checkbox" name="email" tabindex="0" class="hidden" />
+              <label>VIA EMAIL</label>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="subscribe">
-        <button type="submit" class="btn btn-primary">SUBSCRIBE</button>
-        <router-link to="/policy" class="item"
-          ><p>Our privacy policy</p></router-link
-        >
-      </div>
-    </form>
+        <div class="subscribe">
+          <button type="submit" class="btn btn-primary">SUBSCRIBE</button>
+          <router-link to="/policy" class="item"
+            ><p>Our privacy policy</p></router-link
+          >
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -165,8 +173,9 @@ h3 {
 }
 
 .form-control {
-  background: rgba(247, 247, 247, 0.3);
+  background: rgba(247, 247, 247, 0.115);
   height: 50px;
+  color: white;
 }
 
 .ui.segment {
@@ -199,5 +208,9 @@ p[data-v-6f83b81c] {
 
 .item {
   text-decoration: none;
+}
+
+.newsBanner {
+  width: 100vh;
 }
 </style>
